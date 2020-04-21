8daeff6fad40ae1efbb131e92d33281f6ad41101
FGEbba <54099779+FGEbba@users.noreply.github.com>
2020-04-21 14:15:02 +0200

    Forgot to push this earlier. SHits done tho.

---
+++ Assets/Scripts/ThingMover.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class ThingMover : MonoBehaviour
+{
+    private float moveSpeed = 1f;
+    private float Timer = 0;
+    private int currentNode;
+    private Vector3 CurrentPositionHolder;
+    private Vector3 startPosition;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Timer += Time.deltaTime * moveSpeed;
+        if(transform.position != CurrentPositionHolder)
+        {
+            transform.position = Vector3.Lerp(startPosition, CurrentPositionHolder, Timer);
+        }
+        else
+        {
+            if(currentNode < Linedrawer.points.Count - 1)
+            {
+                currentNode++;
+                Timer = 0;
+                startPosition = transform.position;
+                CurrentPositionHolder = Linedrawer.points[currentNode].transform.position;
+            }
+            else if (currentNode >= Linedrawer.points.Count - 1)
+            {
+                currentNode = 0;
+                Timer = 0;
+                startPosition = transform.position;
+                CurrentPositionHolder = Linedrawer.points[currentNode].transform.position;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/WaypointThing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointThing : MonoBehaviour
+{
+    [SerializeField] private Color colour = Color.white;
+    [SerializeField] private float sphereRadius = 0f;
+
+
+    public void SetSphereRadius(float newSphereRadius)
+    {
+        sphereRadius = newSphereRadius;
+    }
+
+
+
+    public void SetColour(Color newColour)
+    {
+        colour = newColour;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = colour;
+        Gizmos.DrawSphere(transform.position, sphereRadius);
+
+    }
+
+
+}
