053827a3215e0d14edd1f509533a04ebde85edfa
FGEbba <54099779+FGEbba@users.noreply.github.com>
2020-08-06 15:11:21 +0200

    Redid the whole assignment. Got help from a friend-

---
+++ Assets/Scripts/Editor/WaypointInspector.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WaypointThing))]
+public class WaypointInspector : Editor
+{
+    WaypointThing Point;
+
+    protected static bool ShowColorsMenu = false;
+    protected static bool ShowLineMenu = false;
+    protected static bool ShowGUIMenu = false;
+    protected static bool ShowIndexManipulate = false;
+
+    #region SerializedProperties
+    private SerializedObject serializedTarget;
+
+    private SerializedProperty isLoop;
+    private SerializedProperty listIndex;
+
+    private SerializedProperty lineColor;
+    private SerializedProperty gizmoColor;
+
+    private SerializedProperty lineType;
+    private SerializedProperty lineSpacing;
+
+    private SerializedProperty fontSize;
+    private SerializedProperty gizmoSize;
+
+    private SerializedProperty PointList;
+    #endregion
+
+    private void OnEnable()
+    {
+        Point = (WaypointThing)target;
+
+        SceneView.duringSceneGui += DuringSceneGUI;
+
+        serializedTarget = new SerializedObject(Point);
+
+        isLoop = serializedTarget.FindProperty("LoopWaypoints");
+        listIndex = serializedTarget.FindProperty("WaypointNumber");
+
+        lineColor = serializedTarget.FindProperty("LineColors");
+        gizmoColor = serializedTarget.FindProperty("GizmoColors");
+
+        lineType = serializedTarget.FindProperty("SelectedType");
+        lineSpacing = serializedTarget.FindProperty("DottedLineSpacing");
+
+        fontSize = serializedTarget.FindProperty("FontSize");
+        gizmoSize = serializedTarget.FindProperty("WaypointGizmoSize");
+
+        PointList = serializedTarget.FindProperty("Waypoints");
+    }
+
+    private void OnDisable() => SceneView.duringSceneGui -= DuringSceneGUI;
+
+    public override void OnInspectorGUI()
+    {
+        serializedTarget.Update();
+
+
+        EditorGUILayout.PropertyField(isLoop);
+
+        ShowIndexManipulate = EditorGUILayout.Foldout(ShowIndexManipulate, "Index-based Insert/Remove");
+        if (ShowIndexManipulate)
+        {
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.PropertyField(listIndex);
+            if (GUILayout.Button("Add at Index", GUILayout.Width(120)))
+            {
+                Point.Insert();
+            }
+            if (GUILayout.Button("Remove at Index", GUILayout.Width(120)))
+            {
+                Point.RemoveAt();
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        ShowColorsMenu = EditorGUILayout.Foldout(ShowColorsMenu, "Color Settings");
+        if (ShowColorsMenu)
+        {
+            EditorGUILayout.PropertyField(lineColor);
+            EditorGUILayout.PropertyField(gizmoColor);
+        }
+        ShowLineMenu = EditorGUILayout.Foldout(ShowLineMenu, "Line Settings");
+        if (ShowLineMenu)
+        {
+            EditorGUILayout.PropertyField(lineType);
+            EditorGUILayout.PropertyField(lineSpacing);
+        }
+        ShowGUIMenu = EditorGUILayout.Foldout(ShowGUIMenu, "GUI Settings");
+        if (ShowGUIMenu)
+        {
+            EditorGUILayout.PropertyField(fontSize);
+            EditorGUILayout.PropertyField(gizmoSize);
+        }
+
+        EditorGUILayout.PropertyField(PointList);
+        #region Buttons
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Add Waypoint", GUILayout.Width(120)))
+        {
+            Point.Add();
+        }
+        if (GUILayout.Button("Remove Waypoint", GUILayout.Width(120)))
+        {
+            Point.Remove();
+        }
+        GUILayout.EndHorizontal();
+        #endregion
+
+        serializedTarget.ApplyModifiedProperties();
+    }
+
+    private void DuringSceneGUI(SceneView sceneView)
+    {
+        DrawHandles();
+        if (Event.current.type == EventType.MouseMove) { sceneView.Repaint(); }
+    }
+
+    private void DrawHandles()
+    {
+        for (int i = 0; i < Point.Waypoints.Count; i++)
+        {
+            Handles.color = Point.LineColors;
+            if (Handles.Button(Point.CalculateMidPoint(i), Quaternion.identity, 1f, 1f, Handles.SphereHandleCap)) { Point.Insert(i); }
+        }
+
+        serializedTarget.Update();
+
+        for (int i = 0; i < PointList.arraySize; i++)
+        {
+            if (i > 0) { PointList.GetArrayElementAtIndex(i).vector3Value = Handles.PositionHandle(Point.Waypoints[i], Quaternion.identity); }
+            else { PointList.GetArrayElementAtIndex(i).vector3Value = Point.transform.position; }
+        }
+
+        serializedTarget.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/LineTraveler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineTraveler : MonoBehaviour
+{
+    [SerializeField] private GameObject WaypointCore = null;
+    private WaypointThing WaypointRef = null;
+    [SerializeField] [Unity.Collections.ReadOnly] private int currentPoint = 0;
+    [SerializeField] [Unity.Collections.ReadOnly] private int traversalDir = 1;
+
+    private enum Traversal { None, Travers, PingPong }
+    [SerializeField] private Traversal TraversalType = Traversal.None;
+
+    private void Awake()
+    {
+        if (WaypointCore.GetComponent<WaypointThing>() != null)
+        {
+            WaypointRef = WaypointCore.GetComponent<WaypointThing>();
+        }
+        else { throw new MissingComponentException("No Waypoint core was found " + transform.name); }
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, WaypointRef.Waypoints[currentPoint]) > 0.1f) { transform.position = Vector3.Lerp(transform.position, WaypointRef.Waypoints[currentPoint], Time.deltaTime); }
+        else if (traversalDir == 1 && currentPoint < WaypointRef.Waypoints.Count - 1) { currentPoint += traversalDir; }
+        else if (traversalDir == -1 && currentPoint > 0) { currentPoint += traversalDir; }
+        else { CheckEndTraversal(); }
+    }
+
+    private void CheckEndTraversal()
+    {
+        if (WaypointRef.LoopWaypoints) { currentPoint = 0; }
+        else if (TraversalType == Traversal.PingPong) { traversalDir *= -1; }
+        else { Debug.LogWarning("Unable to travel, no traversal type was selected."); }
+    }
+}
